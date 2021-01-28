aefd608b2d94e6a96e55091b68c9ecacf4114be2
André de Albuquerque <itstheandre@gmail.com>
2021-01-28 20:41:23 +0100

    Lab

--- start/app.js
+++ start/app.js
@@ -81,6 +81,7 @@ class Player {
     this.position = 0;
   }
   move() {
+    console.log("MOVING");
     const numberOfSteps = roleDice();
     const newSpot = this.position + numberOfSteps;
     if (newSpot > BOARD_SIZE) {
@@ -92,22 +93,124 @@ class Player {
   }
 }
 
-class FancyPlayer {
-  constructor(name, fancyHi) {
-    this.name = name;
-    this.fancyHi = fancyHi;
-  }
+const player1 = new Player("brittney");
+const player2 = new Player("aleksandra");
 
-  move() {
-    console.log(`Nope, I'm to damned fancy. Ciao!`);
-  }
-  sayHi() {
-    console.log(this.fancyHi);
+class Cheater extends Player {
+  constructor(name, startingMoney) {
+    super(name);
+    this.position = 10;
+    this.money = startingMoney;
   }
 }
 
-const player1 = new Player("brittney");
-const player2 = new Player("aleksandra");
-const andre = new FancyPlayer("andre", "Yahuga");
-andre.move();
-andre.sayHi();
+const liarCheater = new Cheater("Filipe", 500);
+
+class Human {
+  constructor() {
+    this.lives = 1;
+  }
+
+  screamYolo() {
+    console.log(`YOLO`);
+  }
+}
+
+const human = new Human();
+human.screamYolo();
+
+class NotSmartHuman extends Human {
+  screamYolo() {
+    console.log(
+      `Sounds right, lets try to become target at atomic detonation target practices`
+    );
+  }
+}
+
+const notSoSmart = new NotSmartHuman();
+console.log("notSoSmart:", notSoSmart);
+notSoSmart.screamYolo();
+
+class SmartHuman extends Human {
+  constructor(name) {
+    super();
+    this.name = name;
+  }
+}
+
+class VerySmartHuman extends SmartHuman {
+  saySmartThing() {
+    console.log(`Uuuuh, elementary, uuuh, indeed`);
+  }
+}
+
+const watson = new VerySmartHuman("John Watson");
+console.log("watson:", watson);
+// VerySmartHuman (saySmartThing M., lives P., name P.)
+// Very Smart Human comes from Smart HUman
+// Smart Human (name P., lives P.)
+// Smart Human comes from Human
+// Human (lives P)
+
+// A new property (isStudying)
+class WantsToBeSmarter extends VerySmartHuman {
+  constructor(isStudying, name) {
+    super(name);
+    this.isStudying = isStudying;
+  }
+
+  wantsToParty() {
+    return false;
+  }
+}
+
+// WantsToBeSmarter (saySmartThing M., wantsToParty M., lives P., name P., isStudying P.)
+
+class DoesntHaveTimeToBeSmarter extends VerySmartHuman {
+  isBuildingFuture() {
+    return true;
+  }
+}
+
+// DoesntHaveTimeToBeSmarter (saySmartThing M., isBuildingFuture M., lives P., name P.,)
+
+class Student extends WantsToBeSmarter {
+  // field
+  constructor(name, field) {
+    super(true, name);
+    this.field = field;
+  }
+}
+
+const newStudent = new Student("nelli", "webdev");
+console.log("newStudent:", newStudent);
+
+class Ironhacker extends Student {
+  constructor(name, favoriteLab) {
+    super(name, "webdev");
+    this.favoriteLab = favoriteLab;
+  }
+}
+
+const ironhacker = new Ironhacker("hugo", "functions and arrays");
+const gizem = new Ironhacker("gizem", "clue");
+console.log("gizem:", gizem);
+
+class BreakoutRoom {
+  constructor() {
+    this.room = [
+      new Ironhacker("brittney", "instagram clone"),
+      new Ironhacker("tom", "clue"),
+      new Ironhacker("luca manzo", "css spotify"),
+    ];
+  }
+}
+
+console.log(new BreakoutRoom());
+
+/* 
+   Connect Four
+   class -> Board
+   class (2 instances) -> Player
+   class Little Circles
+*/
